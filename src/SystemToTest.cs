a25df14bf5e88d7b7cfa4b8ffc845344aa9dc081
MarcSau <Marc-Work@DESKTOP-1L66BP0>
2026-01-20 12:39:44 +0100

    Initial Commit

---
+++ src/SystemToTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICD_Test.src
+{
+    static class SystemToTest
+    {
+        public static bool Behaviour1()
+        {
+            return true;
+        }
+
+        public static bool Behaviour2()
+        {
+            return false;
+        }
+    }
+}
---
+++ src/Tester.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICD_Test.src
+{
+    internal class Tester
+    {
+        public bool TestWorkingBehaviours()
+        {
+            return SystemToTest.Behaviour1();
+        }
+
+        public bool TestNoyWorkingBehaviours()
+        {
+            return SystemToTest.Behaviour2();
+        }
+    }
+}
